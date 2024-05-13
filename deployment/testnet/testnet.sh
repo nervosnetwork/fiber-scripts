ed176dfe493072107c19a0eabfee163a30d9bf74
quake <quake.wang@gmail.com>
2024-05-13 13:52:08 +0900

    chore: deploy to testnet

---
+++ deployment/testnet/testnet.sh
@@ -0,0 +1,6 @@
+ckb-cli deploy gen-txs \
+    --deployment-config ./testnet.toml \
+    --migration-dir ./migrations \
+    --from-address ckt1qyqxgftlqzmtv05cwcyl4xlz6ryx6dgsyrasjrp27t \
+    --sign-now \
+    --info-file testnet.json
